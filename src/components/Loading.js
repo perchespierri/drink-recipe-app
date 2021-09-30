9b5dab76024fb9481803e6de80b067218dc35522
perchespierri <perchespierri@gmail.com>
2021-09-30 15:46:01 -0300

    API fetch implemented

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Loading extends Component {
+  render() {
+    return (
+      <p className='loading-paragraph'>Loading ...</p>
+    )
+  }
+}
+
+export default Loading
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Loading from '../components/Loading';
 
 class Main extends Component {
 
   state = {
     query: '',
+    loading: false,
+    cocktails: null,
+    // OBS 3
   };
 
   handleChange = ({target: {name, value}}) => {
@@ -12,19 +16,49 @@ class Main extends Component {
     });
   };
 
+  fetchData = () => {
+    const { query } = this.state;
+    // OBS 2
+    const apiData = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`)
+      .then((response) => response.json())
+      .then((response) => this.setState({
+        cocktails: response.drinks,
+      }))
+      .catch(error => console.error(error));
+    this.setState({loading: false, query:''});
+  };
+
   render() {
-    const {query}= this.state;
+    const {query, loading, cocktails}= this.state;
     return (
-      <div>
-        <label htmlFor='query-input'> {/* obs 1 */}
-          Search by drink:
+      <div className='search-bar'>
+        {loading && <Loading />}
+        <label htmlFor='query-input'> {/* OBS 1 */}
           <input
             type='text'
             id='query-input'
-            value={query}
+            placeholder='Search drink by name'
+            name='query'
+            value={ query }
             onChange={this.handleChange}
           />
         </label>
+        <button
+          type='button'
+          onClick={this.fetchData}
+        >
+          Search
+        </button>
+        {cocktails.map(cocktail => (
+          <>
+            <h1 key={cocktail.idDrink}>{cocktail.strDrink}</h1>
+            <img
+              src={ cocktail.strDrinkThumb }
+              alt={cocktail.strDrink}
+              width="200"
+            />
+          </>
+        ))}
       </div>
     );
   }
@@ -34,4 +68,5 @@ export default Main;
 
 // OBS:
 // 1 - label tag needs htmlFor equal to input's id
-// 2
+// 2 - Always use https:// before endpoint
+// 3 - API returns drinks: null if query doesn't exist
